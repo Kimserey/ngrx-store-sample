758990a408c41cfbe5dcfda6b22b98f86501f962
Kimserey <kimserey.lam@afusion.com>
2017-07-07 09:56:48 +0800

    added effects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { EffectsModule } from '@ngrx/effects';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { StoreModule } from '@ngrx/store';
@@ -7,6 +8,8 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppComponent } from './app.component';
 import { AppService } from './app.service';
+import { GroupEffects } from './effects/group';
+import { UserEffects } from './effects/user';
 import { reducer } from './reducers';
 
 @NgModule({
@@ -19,6 +22,8 @@ import { reducer } from './reducers';
     HttpModule,
     StoreModule.provideStore(reducer),
     StoreDevtoolsModule.instrumentOnlyWithExtension(),
+    EffectsModule.run(GroupEffects),
+    EffectsModule.run(UserEffects)
   ],
   providers: [AppService],
   bootstrap: [AppComponent]
---
+++ src/app/effects/group.ts
@@ -0,0 +1,25 @@
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
+import { Action } from '@ngrx/store';
+import { Effect, Actions, toPayload } from '@ngrx/effects';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+import { AppService } from '../app.service';
+import * as group from '../actions/group';
+
+@Injectable()
+export class GroupEffects {
+  @Effect()
+  load$: Observable<Action> = this.actions$
+    .ofType(group.LOAD)
+    .map(toPayload)
+    .switchMap(payload => {
+      return this.service.getUserProfile(payload)
+        .map(groups => new group.LoadSuccessAction(groups))
+        .catch(() => of(new group.LoadFailAction()));
+    });
+
+  constructor(private actions$: Actions, private service: AppService) { }
+}
