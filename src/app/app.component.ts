4e7023ccac72b2ff77dd66ded027168787032032
Kimserey <Kimserey.lam@groomgy.com>
2017-08-03 23:00:19 +0800

    more

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <app-select-user></app-select-user>
-    <app-user-profile></app-user-profile>
-    <app-groups></app-groups>
+    <router-outlet></router-outlet>
   `
 })
 export class AppComponent { }
